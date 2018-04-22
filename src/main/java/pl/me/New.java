026b1a4200a90d88de0b97494d7085055a4a7966
liwoszka <liwosz@gmail.com>
2018-04-22 12:00:21 +0200

    witaj swiecie 2 raz

--- src/main/java/pl/me/New.java
+++ src/main/java/pl/me/New.java
@@ -3,5 +3,6 @@ package pl.me;
 public class New {
     public static void main(String[] args) {
         System.out.println("witaj swiecie");
+        System.out.println("witaj swiecie 2 raz");
     }
 }
